027a15ec2f5dac1631755f19b28887097d79a4ea
Jack Stanley <jackstanley1000@gmail.com>
2025-04-13 23:14:03 -0400

    Delete node_modules directory

--- node_modules/smart-chat-model/adapters.js
+++
@@ -1,7 +0,0 @@
-const { AnthropicAdapter } = require('./adapters/anthropic');
-const { CohereAdapter } = require('./adapters/cohere');
-const { GeminiAdapter } = require('./adapters/gemini');
-exports.Anthropic = AnthropicAdapter;
-exports.Cohere = CohereAdapter;
-exports.Gemini = GeminiAdapter;
-
